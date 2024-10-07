8e9ce8cde1a57936de4efda2a12d157766cbb459
Mohammad Sameer <ms4492222@gmail.com>
2024-10-07 22:55:07 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl text-red-500 flex justify-center items-center h-screen'>
+     Hello from App !!
+    </div>
+  )
+}
+
+export default App
